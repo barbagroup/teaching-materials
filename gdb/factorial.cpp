6f538fd55fc22e124048866e506b82cb07508949
cdcooper84 <cdcooper@bu.edu>
2014-09-23 15:00:53 -0400

    Update factorial.cpp

--- gdb/factorial.cpp
+++ gdb/factorial.cpp
@@ -4,26 +4,24 @@ int factorial(int N)
 {
     int fact;
 
-	for (int i=1; i<=N; i++)
-	{
-		fact = fact*i;
-	}
+    for (int i=1; i<=N; i++)
+    {
+  	fact = fact*i;
+    }
 
-	return fact;
+    return fact;
 }
 
 int main()
 {
-
-	std::cout << "Enter a positive integer: " << std::endl;
+    std::cout << "Enter a positive integer: " << std::endl;
 
     int num, num_fact;
-	std::cin >> num;
+    std::cin >> num;
 
-	num_fact = factorial(num);
+    num_fact = factorial(num);
 
-	std::cout << num << "!" << " = " << num_fact << std::endl;
+    std::cout << num << "!" << " = " << num_fact << std::endl;
 
-	return 0;
-	
+    return 0;
 }
