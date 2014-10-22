9cfae883ea352dce1f52d22d51bd1afce440c119
mesnardo <mesnardo@gwu.edu>
2014-10-22 09:11:24 -0400

    Add comments to the code

--- Single_Core_Optimization/fast_fourier_transform/code/fft_optimized.cpp
+++ Single_Core_Optimization/fast_fourier_transform/code/fft_optimized.cpp
@@ -1,16 +1,25 @@
+/***************************************************************************//**
+* \file fft_optimized.cpp
+* \author Olivier Mesnard (mesnardo@gwu.edu)
+* \brief Compute the Fast Fourier Transform using a 4-radix implementation
+*		 described by Chellappa et al. (2008)
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
+// define number pi
 #define PI 3.14159265358979323846
 
-// compute the exponent
+// compute the exponent (macro)
 #define log4(N) (int)(log(N)/log(4))
 
 // cache line size = 2 complex numbers (16 bytes)
 #define LS 2
 
-// top-level call to DFT function
+// top-level call to DFT function (macro)
 # define DFT(N, Y, X) DFT_rec(N, log4(N), Y, X, 1)
 
 
@@ -20,20 +29,33 @@ void DFT4_twiddle(double *Y, int s, int N, int j);
 void DFT_rec(int N, int n, double *Y, double *X, int s);
 void DFT_buf_rec(int N, int n, double *Y, double *X, int s, int th);
 
+
+/**
+* \fn void DTF_rec(int N, int n, double *Y, double *X, int s)
+* \brief Computes the DFT recursively with a radix 4.
+*
+* \param N size of the input vector
+* \param n exponent of N in base 4
+* \param Y ouput vector
+* \pram X input vector
+* \param s stride to read the input elements
+*/
 void DFT_rec(int N, int n, double *Y, double *X, int s)
 {
 	int j;
 
-	if (N==4)
+	if (N==4) // recursion leaf
 	{
 		DFT4_base(Y, X, s);
 	}
-	else
+	else // recursive call
 	{
+		// first stage: recursion
 		for (j=0; j<4; j++)
 		{
 			DFT_rec(N/4, n-1, Y+(2*(N/4)*j), X+2*j*s, s*4);
 		}
+		// second stage
 		for (j=0; j<N/4; j++)
 		{
 			DFT4_twiddle(Y+2*j, N/4, n, j);
@@ -41,26 +63,40 @@ void DFT_rec(int N, int n, double *Y, double *X, int s)
 	}
 }
 
+
+/**
+* \fn void DTF_buf_rec(int N, int n, double *Y, double *X, int s, int th)
+* \brief Computes the buffered DFT recursively with a radix 4.
+*
+* \param N size of the input vector
+* \param n exponent of N in base 4
+* \param Y ouput vector
+* \pram X input vector
+* \param s stride to read the input elements
+* \param th threshold to control the sizes 
+*			for which the second loop gets buffered
+*/
 void DFT_buf_rec(int N, int n, double *Y, double *X, int s, int th)
 {
 	int i, j, j1, j2, k;
 	// local buffer
 	double buf[8*LS];
 
-	if (N==4)
+	if (N==4) //recursion leaf
 	{
 		DFT4_base(Y, X, s);
 	}
-	else
+	else // recursive call
 	{
-		if (N>th)
+		// first stage
+		if (N>th) // buffered recursion
 		{
 			for (j=0; j<4; j++)
 			{
 				DFT_buf_rec(N/4, n-1, Y+(2*(N/4)*j), X+2*j*s, s*4, th);
 			}
 		}
-		else
+		else // normal recursion
 		{
 			for (j=0; j<4; j++)
 			{
@@ -68,6 +104,7 @@ void DFT_buf_rec(int N, int n, double *Y, double *X, int s, int th)
 			}
 		}
 
+		// second stage
 		for (j1=0; j1<N/(4*LS); j1++)
 		{
 			// copy 4 chunks of 2*LS double to local buffer
@@ -95,6 +132,15 @@ void DFT_buf_rec(int N, int n, double *Y, double *X, int s, int th)
 	}
 }
 
+
+/**
+* \fn DTF4_base(double *Y, double *X, int s)
+* \brief Implementation of the DFT4
+*
+* \param Y output vector
+* \param X input vector
+* \param s stride to read the input elements
+*/
 void DFT4_base(double *Y, double *X, int s)
 {
 	double t0, t1, t2, t3, t4, t5, t6, t7;
@@ -116,8 +162,15 @@ void DFT4_base(double *Y, double *X, int s)
 	Y[7] = (t6 - t7);
 }
 
+// twiddle table, intitialized by the function init_DFT(N)
 double **DN;
 
+/**
+* \fn void init_DFT(int N)
+* \brief Initializes the twiddle factors
+*
+* \param N size of the input vector
+*/
 void init_DFT(int N)
 {
 	int i, j, k, size_Dj=16, n_max=log4(N);
@@ -137,6 +190,7 @@ void init_DFT(int N)
 	}
 }
 
+// macro to compute complex multiplication
 #define CMPLX_MULT(cr, ci, a, b, idx, s) 	\
 {											\
 	double ar, ai, br, bi;					\
@@ -146,6 +200,15 @@ void init_DFT(int N)
 	ci = ar*bi + ai*br;						\
 }
 
+/**
+* \fn void DTF4_twiddle(double *Y, int s, int n, int j)
+* \brief Performs an iteration of the second stage
+*
+* \param Y output vector
+* \param s stride to define which elements of the ouput vector to compute
+* \param n exponent of the input size in base 4
+* \param j parameter to know which twiddle factor to compute
+*/
 void DFT4_twiddle(double *Y, int s, int n, int j)
 {
 	double t0, t1, t2, t3, t4, t5, t6, t7;
@@ -178,6 +241,13 @@ void DFT4_twiddle(double *Y, int s, int n, int j)
 	Y[6*s+1] = (t6 - t7);
 }
 
+/**
+* \fn void init_input(double *X, int N)
+* \brief Initiliazes the input vector with ones
+*
+* \param X input vector
+* \param N size of the input vector
+*/
 void init_input(double *X, int N)
 {
 	for (int i=0; i<N; i++)
@@ -186,14 +256,19 @@ void init_input(double *X, int N)
 	}
 }
 
+// main
 int main()
 {
-	int N = 64;
+	int N = 64;	// size of the input vector
 
+	// allocation of the input and output vectors in the memory
 	double *X = new double[N];
 	double *Y = new double[N];
 
+	// compute the DFT recursively using a 4-radix implementation
 	DFT(N, Y, X);
 
 	return 0;
 }
+
+// end of file
